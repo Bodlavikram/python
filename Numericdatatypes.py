7c2792681c2e8ae884b8c0eb92245e27cacb654f
Bodla vikram <Bodlavikram@users.noreply.github.com>
2017-09-13 06:38:52 +0530

    Add files via upload
    DATA TYPES IN PYTHON

---
+++ Numericdatatypes.py
@@ -0,0 +1,22 @@
+#first program for begineers
+
+#declare the integer 
+
+a=10   
+print(a)
+
+#complex j is imaginary part
+
+b=5+9j
+print(b)
+
+
+#float data type
+
+c=10.2
+print(c)
+
+
+#type conversation means converting one data type into the other data type
+d=float(a)
+print(d)
