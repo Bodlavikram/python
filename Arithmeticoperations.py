007ad74a4be6edd31f6ac353a97c2de09e200d9e
Bodla vikram <Bodlavikram@users.noreply.github.com>
2017-09-13 21:18:37 +0530

    Add files via upload

---
+++ Arithmeticoperations.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 18:19:40 2017
+
+@author: vikram
+"""
+#arithmtic opeartions 
+a=10
+b=30
+#Addition of two numbers
+c=a+b
+print(c)
+#subtraction of two numbers
+c=a-b
+print(c)
+#division of two numbers
+c=a/b
+print(c)
+#multiplication of two numbers
+c=a*b
+print(c)
+#modules of two numbers
+c=a%b
+print(c)
