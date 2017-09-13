e25e686aeb03acc4988338d61c768bcb35c66d65
Bodla vikram <Bodlavikram@users.noreply.github.com>
2017-09-13 21:17:01 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 18:47:05 2017
+
+@author: vikram
+"""
+#To declare the string
+a="welcome to python programming"
+
+#To print the string
+print(a)
+
+#To count the number of characters
+print(len(a))
+
+#To locate the character in the string
+print(a.index('m'))
+
+#To count the character occured in the string
+print(a.count('p'))
+
+#To reverse the string
+print(a[::-1])
+
+#To print a part of the string
+print(a[0:7])
+
+#To convert the string to uppercase
+print(a.upper())
+
+#we can also convert the string to lower use var.lower()
