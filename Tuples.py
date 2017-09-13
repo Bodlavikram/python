2990b055e0eb2624bd740dac6f9870ede8973425
Bodla vikram <Bodlavikram@users.noreply.github.com>
2017-09-13 06:40:15 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 05:39:16 2017
+
+@author: vikram
+"""
+
+#thid program for begineers
+
+#creating a Tuples 
+task=("wakeup","brush","bath","breakfast","office","drinks")
+print(task)
+
+#Updating the list
+#Tuples are immutuable once they declared cannot be changed
+
+#print the length of the Tuples
+print(len(task))
+
+#concatentation of the two strings 
+lis=("python","IOT","kotlin","java")
+print(task+lis)
+
+
+#print an element from the Tuple
+print(lis[0])
+
+#print a part of the Tuple
+print(lis[0:3])
+
+#print the Tuple multiple times
+print(task*2)
