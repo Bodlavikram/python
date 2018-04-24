a408ed72a0a41da45e67f51eb435deca08eadca6
Bodla vikram <Bodlavikram@users.noreply.github.com>
2018-04-24 16:45:24 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+                                        #Test1 find length on string and count how many "p" and "c" are present in the string
+string ="welcome to python course"
+                                        #printing length of string
+print(len(string))
+                                        #count number of particular letters in the string
+print("Number of letter in 'p' in the string =%d"% string.count("p"))
+print("Number of letter in 'c' in the string =%d"% string.count("c"))
